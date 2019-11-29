490511c2d39836fca388604bb6059928834c7d58
谭乾坤 <1609558084@qq.com>
2019-11-29 20:43:44 +0800

    静态资源

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less' // 引入默认样式
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
