daeb0108fc531c4031133b1407bc461ed5e2253f
谭乾坤 <1609558084@qq.com>
2019-11-29 21:00:34 +0800

    首页及home页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from './../views/login'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    {
+      path: '/',
+      component: login
+    },
+    {
+      path: '/login',
+      component: login
+    },
+    {
+      path: '/home',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './../views/home')
+    }
 
   ]
 })
